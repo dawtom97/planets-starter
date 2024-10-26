961fa098f03a71d054061b1a601b9dd6e505357d
dawid <dawtom97@gmail.com>
2024-10-26 09:29:20 +0200

    mobile nav bugfix

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const mobileMenu = document.getElementById("MobileNav");
 hamburgerBtn.onclick = function () {
     mobileMenu.classList.toggle("is-visible");
 }
+
+document.onclick = function (event) {
+    if (!mobileMenu.contains(event.target) && event.target.id !== "HamburgerBtn") {
+        mobileMenu.classList.remove("is-visible");
+    }
+}
